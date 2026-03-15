8c490864549dd57ee62d997e6fe3042f90d19d3c
Mathilde Cotelle <mathildecotelle@orange.fr>
2026-03-15 19:07:55 +0100

    Commit de fin de projet

--- index.php
+++ index.php
@@ -7,13 +7,13 @@ require_once 'classes/Library.php';
 
 $library = new Library();
 
-$book1 = new Book("image", "1984", " George Orwell");
+$book1 = new Book("1984_cover.webp", "1984", " George Orwell");
 $book1->setPageCount(328);
-$magazine1 = new Magazine("image", "National Geographic", " Divers");
+$magazine1 = new Magazine("national_geographic_cover.jpg", "National Geographic", " Divers");
 $magazine1->setIssueNumber(150);
-$book2 = new Book("image", "La ligne verte", " Stephen King");
+$book2 = new Book("ligne_verte_cover.jpg", "La ligne verte", " Stephen King");
 $book2->setPageCount(1065);
-$magazine2 = new Magazine("image", "Ca m'intéresse", " Prisma média");
+$magazine2 = new Magazine("ca_minteresse_cover.jpg", "Ca m'intéresse", " Prisma média");
 $magazine2->setIssueNumber(203);
 
 
