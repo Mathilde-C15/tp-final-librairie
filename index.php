2e2b713bba5a9bdf4937a2f5ab516e75551736b3
Mathilde Cotelle <mathildecotelle@orange.fr>
2026-03-15 16:44:21 +0100

    visuel css

--- index.php
+++ index.php
@@ -5,32 +5,15 @@ require_once 'classes/Magazine.php';
 require_once 'classes/Book.php';
 require_once 'classes/Library.php';
 
-// $test = new Magazine("image", "titre", "auteur");
-
-
-// $test->setIssueNumber(14);
-// // echo $test->getTitle();
-// // echo $test->getImage();
-// // echo $test->getAuthor();
-// // echo $test->getInfo();
-
-
-
-// $librairie->addItem($test);
-
-// // echo "<br>";
-// // echo Library::$totalItems;
-
-
 $library = new Library();
 
-$book1 = new Book("image", "1984", "George Orwell");
+$book1 = new Book("image", "1984", " George Orwell");
 $book1->setPageCount(328);
-$magazine1 = new Magazine("image", "National Geographic", "Divers");
+$magazine1 = new Magazine("image", "National Geographic", " Divers");
 $magazine1->setIssueNumber(150);
-$book2 = new Book("image", "La ligne verte", "Stephen King");
+$book2 = new Book("image", "La ligne verte", " Stephen King");
 $book2->setPageCount(1065);
-$magazine2 = new Magazine("image", "Ca m'intéresse", "Prisma média");
+$magazine2 = new Magazine("image", "Ca m'intéresse", " Prisma média");
 $magazine2->setIssueNumber(203);
 
 
@@ -41,5 +24,5 @@ $library->addItem($magazine2);
 
 
 $title = "Index";
-$template = "index.phtml";
-include "template/index.phtml";
+$template = "template/index.phtml";
+include "template/layout.phtml";
